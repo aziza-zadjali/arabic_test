bbd5d4fd5398584036610af03e8fc15655286835
aziza-zadjali <140299461+aziza-zadjali@users.noreply.github.com>
2025-07-07 19:00:59 +0400

    Update question_generator.py

--- question_generator.py
+++ question_generator.py
@@ -1,7 +1,14 @@
-from openai_utils import generate_mcq_arabic_word_meaning, generate_meaning_test_llm
+from openai_utils import (
+    generate_mcq_arabic_word_meaning,
+    generate_meaning_test_llm,
+    generate_mcq_contextual_word_meaning
+)
 
 def create_question(main_word, reference_questions, grade):
     return generate_mcq_arabic_word_meaning(main_word, reference_questions, grade)
 
 def generate_meaning_test(num_questions, reference_questions, grade):
     return generate_meaning_test_llm(num_questions, reference_questions, grade)
+
+def generate_contextual_question(reference_questions, grade):
+    return generate_mcq_contextual_word_meaning(reference_questions, grade)
